fb089b5df17c82cc22730ef42b602037920f1cc0
IJTaladrid <you@example.com>
2019-11-01 13:42:34 -0300

    Task 3: linking MongoDB to local Express instance / https://www.youtube.com/watch?v=5yTazHkDR4o

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://IJTaladrid:Peligro07%43@ijtcluster-5odss.mongodb.net/test?retryWrites=true&w=majority"
-}
--- server.js
+++ server.js
@@ -1,34 +1,19 @@
 const express = require('express');
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.set('useUnifiedTopology', true);
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-/*app.get('/test', (req, res) => {
-  res.send({ express: 'Hello from Express' });
-});
+mongoose.connect("mongodb+srv://IJTaladrid:NtwecbBxtErqPNkh@ijtcluster-5odss.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
+        .then(() => console.log("MongoDB connected..."))
+        .catch(err => console.log(err));
 
-var router = express.Router();
-
-router.get("/", function(req, res)  {
-  res.send('im the home page!');
-});
-
-router.get('/about', function(req, res) {
-  res.send('im the about page!'); 
-});
-
-app.use('/', router);*/
+app.set("port", process.env.PORT || 3000);
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-const db = require("./config/keys.js").mongoURI;
-
-mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(err => console.log(err));
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
+app.listen(app.get("port"), () =>  {
+  console.log("Listening at: ", app.get("port"));
+});
