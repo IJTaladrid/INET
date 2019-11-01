e80faff3548e2d7205668426a0c210b3b707a16c
IJTaladrid <ij.taladrid@gmail.com>
2019-10-31 21:16:03 -0300

    Basic Server Setup v2

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/test', (req, res) => {
+/*app.get('/test', (req, res) => {
   res.send({ express: 'Hello from Express' });
+});*/
+
+var router = express.Router();
+
+router.get("/", function(req, res)  {
+  res.send('im the home page!');
 });
 
+router.get('/about', function(req, res) {
+  res.send('im the about page!'); 
+});
+
+app.use('/', router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
