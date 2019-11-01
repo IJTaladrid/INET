ed56e8f5df700817f9f6403768c7af93c8a4a3c3
IJTaladrid <ij.taladrid@gmail.com>
2019-10-31 22:00:32 -0300

    linking MongoDB to Express / see https://www.youtube.com/watch?v=5yTazHkDR4o

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://IJTaladrid:Peligro07%43@ijtcluster-5odss.mongodb.net/test?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 /*app.get('/test', (req, res) => {
   res.send({ express: 'Hello from Express' });
-});*/
+});
 
 var router = express.Router();
 
@@ -16,7 +19,16 @@ router.get('/about', function(req, res) {
   res.send('im the about page!'); 
 });
 
-app.use('/', router);
+app.use('/', router);*/
+
+app.use(bodyParser.json());
+
+const db = require("./config/keys.js").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
